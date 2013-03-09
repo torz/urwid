c8b46877b2cb15afde393ed41778e3ecce258430
torz <torz@gmx.com>
2013-03-09 21:46:57 +0000

    filelist recovered

--- filelist.py
+++ filelist.py
@@ -2,12 +2,27 @@
 import os
 import urwid
 
-cwd = os.getcwd()
 
-def create_walker_wid(path):
-	
+def create_walker_wid(path=None):
+	# get filelist from path
+	try:
+		listFiles = os.listdir(path)
+	except:
+		listFiles = os.listdir('/')
+	# create empty list for widget list
+	buttonList = []
+	# create button list
+	for oneFile in listFiles:
+		buttonList.append(urwid.Button(oneFile))
+	# pass  button list to simple walker
+	simpleWalker = urwid.SimpleListWalker(buttonList)
+	# return boxList
+	return urwid.ListBox(simpleWalker)
 
-# list to button
-# buttons to simplewalker
-# simplewalkwwer to listbox
-# listbox to main
+def quit_on_q(key):
+	if key == 'q':
+		raise urwid.ExitMainLoop
+
+loop = urwid.MainLoop(create_walker_wid(), unhandled_input=quit_on_q)
+loop.run()
+
