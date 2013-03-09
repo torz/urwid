e166f8c9437a7edcaedb6dd709f6faf4d6af199f
torz <torz@gmx.com>
2013-03-09 16:51:08 +0000

    restore more

--- filelist.py
+++ filelist.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 import os
 import urwid
+
+cwd = os.getcwd()
+
+def create_walker_wid(path):
+	
+
+# list to button
+# buttons to simplewalker
+# simplewalkwwer to listbox
+# listbox to main
