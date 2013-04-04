a43359c9b6b3516aa7874ead11f47fb68b34a9b7
torz <torz@gmx.com>
2013-04-04 16:57:31 +0100

    make it a bit nicer

--- filelist2.py
+++ filelist2.py
@@ -3,47 +3,43 @@ import os
 import urwid
 
 currentPath = '/'
-dirList = []
+choices = os.listdir(currentPath)
 
-def update_curent_path(nextPath):
+def update_cp_choices(choice):
     global currentPath
-    if nextPath == '..':
+    global choices
+    if choice == '..':
         currentPath = os.path.dirname(currentPath)
     else:
-        currentPath = os.path.join(currentPath, nextPath)
-    
-def update_dir_list():
-    global currentPath
-    global dirList
-    try:
-        dirList = os.listdir(currentPath)
-    except:
-        currentPath = '/'
-        dirList = os.listdir(currentPath)
+        currentPath = os.path.join(currentPath, choice)
+    if os.path.isdir(currentPath):
+        choices = os.listdir(currentPath)
     if currentPath != '/':
-        dirList.append('..')
+        choices.insert(0, '..')
 
-def create_buttons():
-    global dirList
-    # create empty list for widget list
-    buttonList = []
-    # create button list
-    for oneFile in dirList:
-        button = urwid.Button(oneFile)
-        urwid.connect_signal(button, 'click', create_main_wid, oneFile)
-        buttonList.append(button)
-    # pass  button list to simple walker
-    return urwid.ListBox(urwid.SimpleListWalker(buttonList))
+def menu(title, choices):
+    global currentPath
+    body = [urwid.Text(title), urwid.Divider()]
+    for c in choices:
+        button = urwid.Button(c)
+        if os.path.isdir(os.path.join(currentPath, c)):
+            urwid.connect_signal(button, 'click', update_choices, c)
+        body.append(urwid.AttrMap(button, None, focus_map='reversed'))
+    return urwid.ListBox(urwid.SimpleFocusListWalker(body))
 
-def quit_on_q(self, key):
-    if key == 'q':
-        raise urwid.ExitMainLoop
+def exit_program(button):
+    if button == 'q':
+        raise urwid.ExitMainLoop()
 
-def main():
-    update_dir_list()
+def update_choices(button, choice):
+    global choices
+    update_cp_choices(choice)
+    main.original_widget = menu(title, choices)
 
-    loop = urwid.MainLoop(mainWid, unhandled_input=quit_on_q)
-    loop.run()
-
-if __name__ == '__main__':
-    main()
+title = u'File Browser - Press q to quit'
+main = urwid.Padding(menu(title, choices), left=9, right=9)
+top = urwid.Overlay(main, urwid.SolidFill(u'\N{MEDIUM SHADE}'),
+    align='center', width=('relative', 60),
+    valign='middle', height=('relative', 60),
+    min_width=20, min_height=9)
+urwid.MainLoop(top, palette=[('reversed', 'standout', '')], unhandled_input=exit_program).run()
